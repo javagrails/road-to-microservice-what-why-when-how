26e080d41496da15e6d1e0b6354f0b7de7e40c3c
salman_at_github <salman1m22@gmail.com>
2020-04-16 08:33:06 +0600

    ms-one-bank-b microservice added also a common packaging added in both a and b microservice

---
+++ ms-two-bank-b/src/main/java/bkorg/mstwobankb/app/MsTwoBankBApplication.java
@@ -0,0 +1,13 @@
+package bkorg.mstwobankb.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsTwoBankBApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsTwoBankBApplication.class, args);
+    }
+
+}
---
+++ ms-two-bank-b/src/test/java/bkorg/mstwobankb/app/MsTwoBankBApplicationTests.java
@@ -0,0 +1,13 @@
+package bkorg.mstwobankb.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsTwoBankBApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
